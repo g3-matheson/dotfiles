a51b23529ab830e76604aa6697f660b2a6225dfe
g3-matheson <g3.matheson@gmail.com>
2025-12-20 04:10:09 -0500

    <leader>tv for vertical terminal... weird merge issues

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
----- Vertical terminal toggle
+-- Vertical terminal toggle (pure Neovim, no Snacks)
 vim.keymap.set("n", "<leader>tv", function()
-  Snacks.terminal(nil, {
-    win = { position = "right" },
-  })
-end, { desc = "Terminal (vertical)" })
-
+  vim.cmd("vsplit")
+  vim.cmd("wincmd L")
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Terminal (vertical right)" })
