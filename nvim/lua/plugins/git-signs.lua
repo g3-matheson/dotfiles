cd24481c654ec57708ed2b01d102033c4cca5a02
g3-matheson <g3.matheson@gmail.com>
2025-12-22 03:24:21 -0500

    git signs update

--- nvim/lua/plugins/git-signs.lua
+++ nvim/lua/plugins/git-signs.lua
@@ -2,20 +2,23 @@ return {
   "lewis6991/gitsigns.nvim",
   opts = {
     signs = {
-      --    󰜺 󰃢
-      add = { text = "" },
-      change = { text = "" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "" },
+      -- usable icons:
+      --    󰜺 󰃢      󰯙 󰜥 ♥  󰇾  󰓛 󰆕  󰐃     󰿶   󰙂 󰿦 󱝊 󰈸  󰱥 󰆖 󱐋        󰈻 󰼠 󰋠
+      --     󱓇 󰑀 󰑂 󰑅 󰝤 󰞋   󰓡 
+      -- 󱪼 󱪽 󱪾 󱪿 󱤩 󰒆 󰾂 󰒇   󰜅 󰨓 f 󰀧 󰨔 󱖢  █
+      add = { text = "󰿶" },
+      change = { text = "󰜥" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "" },
     },
     signs_staged_enable = true, -- Show staged changes
     signs_staged = {
-      add = { text = "█" },
-      change = { text = "█" },
-      delete = { text = "█" },
-      topdelete = { text = "█" },
-      changedelete = { text = "█" },
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "┃" },
+      topdelete = { text = "▔" },
+      changedelete = { text = "┃" },
     },
   },
 }
