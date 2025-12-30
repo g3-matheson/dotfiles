5d9cc388ee916a07d5970c2e9146e55df62207cb
g3-matheson <g3.matheson@gmail.com>
2025-12-29 22:01:55 -0500

    vimtex config

---
+++ nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,28 @@
+return {
+  "lervag/vimtex",
+  lazy = false, -- lazy-loading will disable inverse search
+  config = function()
+    -- Compiler settings for XeLaTeX or LuaLaTeX
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = "",
+      callback = 1,
+      executable = "latexmk",
+      hooks = {},
+      options = {
+        "-xelatex", -- Use XeLaTeX
+        -- '-lualatex',  -- Uncomment for LuaLaTeX instead
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+      },
+    }
+
+    -- PDF Viewer settings
+    vim.g.vimtex_view_method = "zathura"
+
+    -- Turn off concealing
+    vim.g.vimtex_syntax_conceal_disable = 1
+  end,
+}
