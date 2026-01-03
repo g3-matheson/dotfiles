eb27c2acdd5ae64c4212779eb8550dddecb8bf2a
g3-matheson <g3.matheson@gmail.com>
2026-01-03 03:04:39 -0500

    neovim horizontal terminal keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,7 +5,18 @@
 -- Vertical terminal toggle (pure Neovim, no Snacks)
 vim.keymap.set("n", "<leader>tv", function()
   vim.cmd("vsplit")
-  vim.cmd("wincmd L")
+  -- vim.cmd("wincmd L")
   vim.cmd("terminal")
   vim.cmd("startinsert")
 end, { desc = "Terminal (vertical right)" })
+
+-- Horizontal terminal toggle (pure Neovim, no Snacks)
+vim.keymap.set("n", "<leader>th", function()
+  vim.cmd("split")
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Terminal (horizontal bottom)" })
+
+-- Window navigation
+vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window" })
