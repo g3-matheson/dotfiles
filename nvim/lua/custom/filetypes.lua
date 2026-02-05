106e7280f56c48e8bcd6bfa404fe1e678324f374
Kat Matheson <g3.matheson@gmail.com>
2026-02-05 18:52:55 -0500

    add .tpp to cpp clang LSP

--- nvim/lua/custom/filetypes.lua
+++ nvim/lua/custom/filetypes.lua
@@ -1,6 +1,14 @@
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.tpp",
   callback = function()
-    vim.bo.filetype = "cpp"  -- treat .tpp as C++
+    vim.bo.filetype = "cpp" -- treat .tpp as C++
   end,
 })
+
+-- Recognize .tpp files as C++
+vim.filetype.add({
+  extension = {
+    tpp = "cpp",
+  },
+})
+
