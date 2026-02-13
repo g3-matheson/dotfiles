82a9bd6f9da97b3a4c5c2b17daa058e91002c911
Kat Matheson <g3.matheson@gmail.com>
2026-02-13 11:16:52 -0500

    add TEST todo comment to search functionality

---
+++ nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    optional = true,
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME", "TEST" } }) end, desc = "Todo/Fix/Fixme/Test" },
+    },
+  },
+}
