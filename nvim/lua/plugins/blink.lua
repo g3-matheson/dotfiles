6deed71b77b7baa98648d70eadec218d1f530d91
Kat Matheson <g3.matheson@gmail.com>
2026-05-05 13:51:52 -0400

    added markdown rendering

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -5,5 +5,14 @@ return {
       preset = "default",
       ["<C-Space>"] = { "select_and_accept" },
     },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "markdown" },
+      providers = {
+        markdown = {
+          name = "RenderMarkdown",
+          module = "render-markdown.integ.blink",
+        },
+      },
+    },
   },
 }
