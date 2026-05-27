752ef6e58769ddcc6c2cbab5b358cdc9c3ff71c7
Kat Matheson <g3.matheson@gmail.com>
2026-05-27 11:21:39 -0400

    correct nvim/scripts/clangd-wrapper to .gitignore

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -16,3 +16,7 @@ vim.opt.showtabline = 0
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.showbreak = "↪ "
+
+-- Use only .git and cwd for root detection; default also includes "lua" which
+-- causes ~/dotfiles/nvim to become the root when editing nvim config files.
+vim.g.root_spec = { ".git", "cwd" }
